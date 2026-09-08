c407278039472e678641e50903aaf70d9e17dbf5
jungyunmok <jungyunmok@gmail.com>
2026-09-08 16:52:02 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 1.09 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/README.md
+++ 프로그래머스/0/181830. 정사각형으로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정사각형으로 만들기 - 181830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181830?language=java) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 0.15 ms
+메모리: 78.8 MB, 시간: 1.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 08일 16:44:28
+2026년 09월 08일 16:51:58
 
 ### 문제 설명
 
--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,18 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int[][] solution(int[][] arr) {
-        int[][] answer;
         int row = arr.length;
         int column = arr[0].length;
+        
+        if(row == column) return arr;
+        
         int max = Math.max(row, column);
-        if(row != column) {
-            answer = new int[max][max];
-            for(int i=0; i<row; i++) {
-                answer[i] = Arrays.copyOf(arr[i], max);
-            }
-        } else {
-            return arr;
+        int[][] answer = new int[max][max];
+        
+        for(int i=0; i<row; i++) {
+            System.arraycopy(arr[i], 0, answer[i], 0, column);
         }
         return answer;
     }
